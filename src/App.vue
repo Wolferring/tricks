3b44386e7a5cd60d0760fbf7eba764a1850b4dc3
unknown <whimsy@eptonic.com>
2017-10-23 16:57:16 +0800

    modify input component

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       
     </navigation>
     <div class="view main-view">
-      <transition name="fade-quick" mode="out-in">
+      <transition :name="transition" mode="out-in">
         <router-view/>
       </transition>
     </div>
@@ -16,7 +16,18 @@ import navigation from '@/components/navigation.vue'
 export default {
   data () {
     return {
-
+      transition: 'fade-quick'
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      const toDepth = to.path.split('/').length
+      const fromDepth = from.path.split('/').length
+      if (toDepth === fromDepth) {
+        this.transition = 'fade-quick'
+        return false
+      }
+      this.transition = toDepth < fromDepth ? 'slide-right' : 'slide-left'
     }
   },
   components: {
